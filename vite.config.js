ee670c47ef21990f93799d3bb59e3e9e4805cf96
peachfruittea <chocotin1624@gmail.com>
2026-04-21 18:42:12 +0800

    chore: 設定路徑別名@(vite + jsconfig)，簡化import路徑

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   // eslint-disable-next-line no-undef
   base: process.env.NODE_ENV === "production" ? "/Hikayori/" : "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
+    },
+  },
 });
