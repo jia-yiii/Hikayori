08110a7c39ea04b3a7e9c698f34a763eb9bb5d6e
peachfruittea <chocotin1624@gmail.com>
2026-04-28 13:47:35 +0800

    feat: 新增商品卡片

--- src/pages/products/ProductsPage.jsx
+++ src/pages/products/ProductsPage.jsx
@@ -6,6 +6,22 @@ function ProductsPage() {
           <h2>ProductsPage</h2>
         </div>
       </section>
+      <section>
+        <div className="container">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">日系簡約｜短方圓</h5>
+              <p className="card-text">
+                溫柔奶茶色調搭配低飽和暈染，適合日常、約會與通勤穿搭。
+              </p>
+              <a href="#" className="btn btn-primary">
+                詳細資訊
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
