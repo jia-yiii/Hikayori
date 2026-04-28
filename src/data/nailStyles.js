37ae26cc6c00161b86330f414f4cd49a82bc4788
peachfruittea <chocotin1624@gmail.com>
2026-04-28 18:32:11 +0800

    feat: 新增商品資料-第二張圖

--- src/data/nailStyles.js
+++ src/data/nailStyles.js
@@ -1,7 +1,11 @@
 import white from "@/assets/nails/white.png";
+import whiteHand from "@/assets/nails/white_hand.png";
 import pink from "@/assets/nails/pink.png";
+import pinkHand from "@/assets/nails/pink_hand.png";
 import orange from "@/assets/nails/orange.png";
+import orangeHand from "@/assets/nails/orange_hand.png";
 import brownBlue from "@/assets/nails/brownBlue.png";
+import brownBlueHand from "@/assets/nails/brownBlue_hand.png";
 
 const products = [
   {
@@ -12,7 +16,7 @@ const products = [
     shape: "橢圓",
     description:
       "以柔和裸透底色融合細緻珠光光澤，呈現如月光般溫潤低調的質感。整體風格乾淨純粹，帶有微微細閃，在不同光線下散發自然光暈，提升手部氣質。橢圓杏仁型修飾指型，使手指看起來更加纖細優雅，同時兼具日常舒適與精緻感。適合上班、約會或日常穿搭，是一款百搭不退流行的氣質款穿戴甲。",
-    img: white,
+    images: [white, whiteHand],
   },
   {
     id: 2,
@@ -22,27 +26,27 @@ const products = [
     shape: "方圓",
     description:
       "以柔霧裸粉為基底，融入細緻珠光微閃，在光線下呈現如晨霧灑落暖光般的柔和光澤。整體色調溫潤自然，不張揚卻極具氣質，適合日常與各式穿搭風格。短方圓甲型設計提升配戴舒適度，同時修飾指型，讓雙手看起來更加乾淨柔和。是一款兼具質感與實用性的百搭文青款穿戴甲。",
-    img: pink,
+    images: [pink, pinkHand],
   },
   {
     id: 3,
-    pd_name: "暖橙日常・柔光呢語",
+    pd_name: "暖橙日常・柔光承雲",
     style: "極致單色",
     length: "中短款",
     shape: "方圓",
     description:
       "以溫潤裸橙色為主調，呈現如日光灑落般的自然光澤，低飽和色系讓整體更顯柔和耐看。簡約無過多裝飾，保留乾淨純粹的質感，適合喜歡自然系風格的日常穿搭。短方圓甲型不僅舒適好活動，也能修飾指型，使雙手看起來更圓潤柔和。是一款輕鬆融入生活、四季皆宜的文青氣質穿戴甲。",
-    img: orange,
+    images: [orange, orangeHand],
   },
   {
     id: 4,
-    pd_name: "秋霧拾光・咖藍呢喃",
+    pd_name: "秋霧拾光・咖藍私語",
     style: "質感暈染",
     length: "短款",
     shape: "方圓",
     description:
       "以深淺交錯的咖啡與霧藍暈染，營造如秋日霧氣般的柔和層次感，搭配低調米白點綴，整體呈現沉穩又溫柔的氣質。每片甲面皆帶有自然流動的紋理，如同手作般獨一無二。短方圓甲型設計提升日常配戴的舒適度，同時修飾指型，讓手部線條更顯柔和。適合喜愛低調質感與文青風格的日常穿搭，是一款耐看且富有氛圍感的穿戴甲。",
-    img: brownBlue,
+    images: [brownBlue, brownBlueHand],
   },
 ];
 
